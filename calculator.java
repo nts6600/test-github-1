688fb977cbeee9b8ab72332f8bf88be951e787d4
nts6600 <nts6600@gmail.com>
2018-11-23 20:12:37 +0900

    택민이 짱짱맨

--- calculator.java
+++ calculator.java
@@ -1,19 +1,19 @@
 public class Calculator() {
-
+// 택민이 짱짱맨
   public int sum(int a, int b) {
     return a + b;
   }
-
+  // 택민이 짱짱맨
   public int minus(int a, int b) {
     return a - b;
   }
-
+// 택민이 짱짱맨
   public int multiple(int a, int b) {
     return a * b;
   }
-
+// 택민이 짱짱맨
   public int divide(int a, int b) {
     return a / b;
   }
-
+// 택민이 짱짱맨
 }
